c694dbb5766ce1cd7032accc8d57207342b8e52d
Nana <nanasobansode4@gmail.com>
2021-03-18 18:02:45 +0530

    added into Remote repository

---
+++ Examination/src/Dept/Computer.java
@@ -0,0 +1,10 @@
+package Dept;
+
+public class Computer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("In computer Dept.");
+	}
+
+}
---
+++ Examination/src/Dept/Mech.java
@@ -0,0 +1,7 @@
+package Dept;
+
+public class Mech {
+public static void main(String[] args) {
+	System.out.println("In mech Dept.");
+}
+}
