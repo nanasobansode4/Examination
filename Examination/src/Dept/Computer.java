078a956db9fb6ec64795df11fc1e4870cf9a15c5
Nana <nanasobansode4@gmail.com>
2021-03-18 18:04:30 +0530

    changes in computer.java file

--- Examination/src/Dept/Computer.java
+++ Examination/src/Dept/Computer.java
@@ -5,6 +5,7 @@ public class Computer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("In computer Dept.");
+System.out.println("welcome to computer dept");
 	}
 
 }
